a2f88f07bf95d53b7932d817ea2963cad810e7a3
Ankit Aggarwal <ankit.spd@gmail.com>
2016-05-12 21:07:55 +0530

    Simplify example

--- Sources/SpawnDemo/main.swift
+++ Sources/SpawnDemo/main.swift
@@ -1,7 +1,7 @@
 import Spawn
 
 do {
-    let spawn = try Spawn(args: ["/bin/sh", "-c", ["ls", "/"].joined(separator: " ")]) { str in 
+    let spawn = try Spawn(args: ["/bin/sh", "-c", "ls", "."]) { str in 
         print(str)
     }
 } catch {
